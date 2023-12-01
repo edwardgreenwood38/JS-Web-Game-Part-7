b810dde83c4624f42800024af4f9d49e181306d9
Edward E Greenwood <edwardgreenwood38@gmail.com>
2023-11-30 19:59:46 -0500

    step 4
    npc just moves south. no movement east

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -27,23 +27,32 @@ function newNonPlayableCharacter(x, y) {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
 
-        sleep(time)
-            .then(() => { stop() } )
+        await sleep(time)
+        stop()
     }
 
-    function walkNorth() {
+    async function walkNorth(time) {
         direction = 'north'
         element.src = `./assets/red-character/north.gif`
+
+        await sleep(time)
+        stop()
     }
 
-    function walkWest() {
+    async function walkWest(time) {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
+
+        await sleep(time)
+        stop()
     }
 
-    function walkSouth() {
+    async function walkSouth(time) {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
+
+        await sleep(time)
+        stop()
     }
 
     function stop() {
